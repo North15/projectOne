2bf338e982959629ea1cc8a4d166c3abdce7e428
Jacob North <15north97@gmail.com>
2018-04-11 02:25:40 -0400

    Updated the file system

---
+++ The-Code/WhitespaceInterpreter/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace WhitespaceInterpreter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.filePath = new System.Windows.Forms.TextBox();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.mainText = new System.Windows.Forms.RichTextBox();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.clearAllButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // filePath
+            // 
+            this.filePath.Location = new System.Drawing.Point(12, 33);
+            this.filePath.Name = "filePath";
+            this.filePath.Size = new System.Drawing.Size(444, 22);
+            this.filePath.TabIndex = 0;
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(462, 32);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(43, 23);
+            this.browseButton.TabIndex = 1;
+            this.browseButton.Text = "...";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // mainText
+            // 
+            this.mainText.Location = new System.Drawing.Point(12, 72);
+            this.mainText.Name = "mainText";
+            this.mainText.Size = new System.Drawing.Size(332, 438);
+            this.mainText.TabIndex = 2;
+            this.mainText.Text = "";
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(362, 72);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(128, 23);
+            this.convertButton.TabIndex = 3;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // clearAllButton
+            // 
+            this.clearAllButton.Location = new System.Drawing.Point(362, 150);
+            this.clearAllButton.Name = "clearAllButton";
+            this.clearAllButton.Size = new System.Drawing.Size(128, 23);
+            this.clearAllButton.TabIndex = 4;
+            this.clearAllButton.Text = "Clear All";
+            this.clearAllButton.UseVisualStyleBackColor = true;
+            this.clearAllButton.Click += new System.EventHandler(this.clearAllButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(362, 111);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(128, 23);
+            this.saveButton.TabIndex = 5;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Filename:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 528);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.clearAllButton);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.mainText);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.filePath);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Whitespace Interpreter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox filePath;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.RichTextBox mainText;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.Button clearAllButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
